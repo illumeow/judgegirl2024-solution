848d16ee4ae0422278c76977ceed6d336dc46faa
illumeow <illumeow0223@gmail.com>
2024-11-12 02:00:10 +0800

    preprocess words, still TLE

--- Exam2021/50215.c
+++ Exam2021/50215.c
@@ -2,16 +2,14 @@
 #include <string.h>
 #include <stdint.h>
 
-void solve(int idx, char words[][55], int n, int letters, int covered, int cost, int *mincost){
-    // printf("%s ", covered_letters);
-    // printf("%d %d min: %d\n", strlen(covered_letters), covered, *mincost);
+void solve(int idx, char words[][55], int wordcost[], int n, int letters, int covered, int cost, int *mincost){
     if(idx == n){
         if(covered >= 26) *mincost = (cost < *mincost)? cost: *mincost;
         return;
     }
 
     // dont choose
-    solve(idx+1, words, n, letters, covered, cost, mincost);
+    solve(idx+1, words, wordcost, n, letters, covered, cost, mincost);
     // choose words[idx]
     int tmp = letters;
     for(int i = 0; words[idx][i] != '\0'; i++){
@@ -20,9 +18,8 @@ void solve(int idx, char words[][55], int n, int letters, int covered, int cost,
             letters |= mask;
             covered++;
         }
-        cost += words[idx][i] - 'a' + 1;
     }
-    solve(idx+1, words, n, letters, covered, cost, mincost);
+    solve(idx+1, words, wordcost, n, letters, covered, cost + wordcost[idx], mincost);
     letters = tmp;
 }
 
@@ -31,8 +28,24 @@ int main(){
     while(scanf("%d", &n) == 1){
         char words[20][55];
         for(int i = 0; i < n; i++) scanf("%s", words[i]);
+        int wordcost[20] = {};
+        for(int i = 0; i < n; i++){
+            char tmp[55];
+            int used = 0;
+            int idx = 0;
+            for(int j = 0; words[i][j] != '\0'; j++){
+                wordcost[i] += words[i][j] - 'a' + 1;
+                int mask = (1 << (words[i][j] - 'a'));
+                if((used & mask) == 0){
+                    used |= mask;
+                    tmp[idx++] = words[i][j];
+                }
+            }
+            tmp[idx] = '\0';
+            strcpy(words[i], tmp);
+        }
         int min = INT32_MAX;
-        solve(0, words, n, 0, 0, 0, &min);
+        solve(0, words, wordcost, n, 0, 0, 0, &min);
         printf("%d\n", min);
     }
 }
