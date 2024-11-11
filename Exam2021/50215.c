045166944b07d7d53adf9c1c793378e0c490cc7a
illumeow <illumeow0223@gmail.com>
2024-11-12 02:24:40 +0800

    another preprocess method, 50215.c AC

--- Exam2021/50215.c
+++ Exam2021/50215.c
@@ -2,25 +2,18 @@
 #include <string.h>
 #include <stdint.h>
 
-void solve(int idx, char words[][55], int wordcost[], int n, int letters, int covered, int cost, int *mincost){
+void solve(int idx, int wordint[], int wordcost[], int n, int used_letters, int cost, int *mincost){
     if(idx == n){
-        if(covered >= 26) *mincost = (cost < *mincost)? cost: *mincost;
+        if(__builtin_popcount(used_letters) >= 26) *mincost = (cost < *mincost)? cost: *mincost;
         return;
     }
 
     // dont choose
-    solve(idx+1, words, wordcost, n, letters, covered, cost, mincost);
+    solve(idx+1, wordint, wordcost, n, used_letters, cost, mincost);
     // choose words[idx]
-    int tmp = letters;
-    for(int i = 0; words[idx][i] != '\0'; i++){
-        int mask = (1 << (words[idx][i] - 'a'));
-        if((letters & mask) == 0){
-            letters |= mask;
-            covered++;
-        }
-    }
-    solve(idx+1, words, wordcost, n, letters, covered, cost + wordcost[idx], mincost);
-    letters = tmp;
+    int tmp = used_letters;
+    solve(idx+1, wordint, wordcost, n, used_letters | wordint[idx], cost + wordcost[idx], mincost);
+    used_letters = tmp;
 }
 
 int main(){
@@ -28,24 +21,15 @@ int main(){
     while(scanf("%d", &n) == 1){
         char words[20][55];
         for(int i = 0; i < n; i++) scanf("%s", words[i]);
-        int wordcost[20] = {};
+        int wordint[20] = {}, wordcost[20] = {};
         for(int i = 0; i < n; i++){
-            char tmp[55];
-            int used = 0;
-            int idx = 0;
             for(int j = 0; words[i][j] != '\0'; j++){
                 wordcost[i] += words[i][j] - 'a' + 1;
-                int mask = (1 << (words[i][j] - 'a'));
-                if((used & mask) == 0){
-                    used |= mask;
-                    tmp[idx++] = words[i][j];
-                }
+                wordint[i] |= (1 << (words[i][j] - 'a'));
             }
-            tmp[idx] = '\0';
-            strcpy(words[i], tmp);
         }
         int min = INT32_MAX;
-        solve(0, words, wordcost, n, 0, 0, 0, &min);
+        solve(0, wordint, wordcost, n, 0, 0, &min);
         printf("%d\n", min);
     }
 }
