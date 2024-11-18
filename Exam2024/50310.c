207fba65b5a2e2ff9f9ebcad4734ce44ae237bd0
illumeow <illumeow0223@gmail.com>
2024-11-18 22:24:59 +0800

    merge ListOfCourse and Classroom

--- Exam2024/50310.c
+++ Exam2024/50310.c
@@ -2,6 +2,12 @@
 #include <stdbool.h>
 
 typedef struct {
+    int capacity;
+    int course_list[8];
+    int course_count;
+} Classroom;
+
+typedef struct {
   int hour;
   int minute;
 } Time;
@@ -13,65 +19,59 @@ typedef struct {
     int day;  // 1: Monday, 2: Tuesday, 3: Wednesday, 4: Thursday, 5: Friday
 } Course;
 
-typedef struct {    
-    int course_count;
-    int course_list[45];
-} ListOfCourse;
-
-bool is_overlap(Course *course, Course *oldCourse){
-    return !(course->end.hour < oldCourse->start.hour ||
-            (course->end.hour == oldCourse->start.hour && course->end.minute < oldCourse->start.minute) ||
-             oldCourse->end.hour < course->start.hour ||
-            (oldCourse->end.hour == course->start.hour && oldCourse->end.minute < course->start.minute));
+bool is_overlap(Course *c1, Course *c2){
+    return !(c1->end.hour < c2->start.hour ||
+            (c1->end.hour == c2->start.hour && c1->end.minute < c2->start.minute) ||
+             c2->end.hour < c1->start.hour ||
+            (c2->end.hour == c1->start.hour && c2->end.minute < c1->start.minute));
 }
 
 int compute_time(Course *c){
     return ((c->end.hour*60+c->end.minute)-(c->start.hour*60+c->start.minute)+1);
 }
 
-bool check(int best[], ListOfCourse list[], int n){
+bool check(int best[], Classroom classrooms[], int n){
     for(int i = 0; i < n; i++){
-        if(best[i] == list[i].course_count) continue;
-        return (best[i] < list[i].course_count);
+        if(best[i] == classrooms[i].course_count) continue;
+        return (best[i] < classrooms[i].course_count);
     }
     return false;
 }
 
-void solve(int idx, int classrooms[], int n, Course courses[], int m, ListOfCourse list[], int nowtime, int remaining_time, int *maxtime, int best[]){
+void solve(int idx, Classroom classrooms[], int n, Course courses[], int m, int nowtime, int remaining_time, int *maxtime, int best[]){
     if(idx == m){
-        if(nowtime > *maxtime || (nowtime == *maxtime && check(best, list, n))){
+        if(nowtime > *maxtime || (nowtime == *maxtime && check(best, classrooms, n))){
             *maxtime = nowtime;
-            for(int i = 0; i < n; i++) best[i] = list[i].course_count;
+            for(int i = 0; i < n; i++) best[i] = classrooms[i].course_count;
         }
         return;
     }
 
     int time = compute_time(&courses[idx]);
     for(int i = 0; i < n; i++){
-        if(classrooms[i] >= courses[idx].number_of_students){
-            ListOfCourse *cur_list = &list[i];
+        if(classrooms[i].capacity >= courses[idx].number_of_students){
             bool overlapped = false;
-            for(int j = 0; j < cur_list->course_count && !overlapped; j++)
-                overlapped = is_overlap(&courses[idx], &courses[cur_list->course_list[j]]);
+            for(int j = 0; j < classrooms[i].course_count && !overlapped; j++)
+                overlapped = is_overlap(&courses[idx], &courses[classrooms[i].course_list[j]]);
                 
             if(!overlapped){
-                cur_list->course_list[cur_list->course_count] = idx;
-                cur_list->course_count++;
-                if(nowtime + remaining_time >= *maxtime) solve(idx+1, classrooms, n, courses, m, list, nowtime+time, remaining_time-time, maxtime, best);
-                cur_list->course_count--;
+                classrooms[i].course_list[classrooms[i].course_count++] = idx;
+                if(nowtime + remaining_time >= *maxtime) solve(idx+1, classrooms, n, courses, m, nowtime+time, remaining_time-time, maxtime, best);
+                classrooms[i].course_count--;
             }
         }
     }
-    if(nowtime + remaining_time >= *maxtime) solve(idx+1, classrooms, n, courses, m, list, nowtime, remaining_time-time, maxtime, best);
+    if(nowtime + remaining_time >= *maxtime) solve(idx+1, classrooms, n, courses, m, nowtime, remaining_time-time, maxtime, best);
 }
 
 int main(){
     int n, m;
     scanf("%d%d", &n, &m);
-    int classrooms[8];
+    Classroom classrooms[8];
+
     Course courses[6][8];
     int coursenum[6] = {}, total_time[6] = {};;
-    for(int i = 0; i < n; i++) scanf("%d", &classrooms[i]);
+    for(int i = 0; i < n; i++) scanf("%d", &classrooms[i].capacity);
     for(int i = 0; i < m; i++){
         int a, b, c, d, e, f;
         scanf("%d %d %d %d %d %d", &a, &b, &c, &d, &e, &f);
@@ -87,13 +87,13 @@ int main(){
     int best[8] = {};
     int maxtime = 0, best_sum = 0;
     for(int i = 1; i <= 5; i++){
-        int maxtime_day = 0, best_day[8] = {};
-        ListOfCourse list[8] = {};
-        solve(0, classrooms, n, courses[i], coursenum[i], list, 0, total_time[i], &maxtime_day, best_day);
-        maxtime += maxtime_day;
+        for(int j = 0; j < n; j++) classrooms[j].course_count = 0;
+        int maxtime_tmp = 0, best_tmp[8] = {};
+        solve(0, classrooms, n, courses[i], coursenum[i], 0, total_time[i], &maxtime_tmp, best_tmp);
+        maxtime += maxtime_tmp;
         for(int i = 0; i < n; i++){
-            best[i] += best_day[i];
-            best_sum += best_day[i];
+            best[i] += best_tmp[i];
+            best_sum += best_tmp[i];
         }
     }
 
