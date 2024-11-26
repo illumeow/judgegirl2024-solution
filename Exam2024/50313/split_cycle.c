f08a653cdb5d1b7371e670e64f6baa375cc35f27
illumeow <illumeow0223@gmail.com>
2024-11-26 17:53:26 +0800

    merge sum into first while

--- Exam2024/50313/split_cycle.c
+++ Exam2024/50313/split_cycle.c
@@ -2,23 +2,16 @@
  
 int split_cycle(struct Node *head, int A, int K){
     struct Node *cur = head->nxt, *tail = head;
-    int size = 1;
+    int size = 1, sum = head->val;
     while(cur != head){
         size++;
+        sum += cur->val;
         tail = cur;
         cur = cur->nxt;
     }
-    if(size <= K){
-        cur = head->nxt;
-        int sum = head->val;
-        while(cur != head){
-            sum += cur->val;
-            cur = cur->nxt;
-        }
-        return sum * A;
-    }
+    if(size <= K) return sum * A;
+    
     int new_idx = (size + 1) / 2;
-    cur = head;
     for(int i = 1; i < new_idx; i++) cur = cur->nxt;
     tail->nxt = cur->nxt;
     cur->nxt = head;
